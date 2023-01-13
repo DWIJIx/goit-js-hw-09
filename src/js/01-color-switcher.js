bda4465ef9a8dacc71b7e4c256dd8a5b135eeb78
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-01-13 10:35:29 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,20 +6,25 @@ const refs = {
 refs.startBtn.addEventListener('click', onStartBtnClick)
 refs.stopBtn.addEventListener('click', onStopBtnClick)
 
+refs.stopBtn.disabled = true;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 function onStartBtnClick() {
     refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
     
+    return timerId = setInterval(() => {
     refs.bodyEl.style.backgroundColor = getRandomHexColor();
-    timerId = setInterval(() => {
-    refs.bodyEl.style.backgroundColor = getRandomHexColor();
-  }, 1000)
+    }, 1000)
+  
 }
 
 function onStopBtnClick() {
     clearInterval(timerId);
     refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
 }
