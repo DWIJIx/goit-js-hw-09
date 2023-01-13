457c43458144219926a5b4a42408b81eeb860388
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-01-13 08:36:02 +0000

    Deploying to gh-pages from @ DWIJIx/goit-js-hw-09@bda4465ef9a8dacc71b7e4c256dd8a5b135eeb78 🚀

--- 01-color-switcher.f6c0261c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={bodyEl:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};function n(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.bodyEl.style.backgroundColor=n(),timerId=setInterval((function(){t.bodyEl.style.backgroundColor=n()}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(timerId),t.startBtn.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.f6c0261c.js.map
